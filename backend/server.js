ab6e0ae1fa750c277d117c9c64966013e0e92d1b
Hannan2004 <2022.abdul.chougle@ves.ac.in>
2025-04-18 20:12:57 +0530

    15th commit

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 connectDB();
 
 const corsOptions = {
-    origin: ['https://task-manager-frontend-470119455745.us-central1.run.app/',
+    origin: ['https://task-manager-frontend-470119455745.us-central1.run.app',
         'http://localhost:3000' ] ,// Replace with your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allow cookies if needed
@@ -20,4 +20,4 @@ app.use(bodyParser.json());
 app.use('/api/tasks', require('./routes/taskRoutes'));
 
 const PORT = 8080;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}.`));
